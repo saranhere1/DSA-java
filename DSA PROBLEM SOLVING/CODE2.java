c93d617ce076427f020f276c53d6dfd4d927bc29
saran <saran122005ravi@gmail.com>
2025-07-02 09:02:02 +0530

    new codes

---
+++ DSA PROBLEM SOLVING/CODE2.java
@@ -0,0 +1,7 @@
+public class CODE2 {
+        public static void main(String[]args){
+            for (int num = 1; num <=5; num++){
+                System.err.println(num);
+            }
+        }
+}
