7133a9415271330f85d6bdcbf3cda9b717c90bf7
saranhere1 <safee000777@gmail.com>
2025-07-07 09:49:11 +0530

    push

---
+++ DSA PROBLEM SOLVING/fib.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fib {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+            int a == 0
+            int b == 1
+            int n == int.next()
+
+while (count <= n){
+    int temp = b
+    b = b + a
+    a = temp
+}
+System.out.println(count);
+    }
+
+}
